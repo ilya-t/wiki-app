e759459b3f74bd548bb01925d785d3ee567ee94c
ilya-t <extraletters@gmail.com>
2021-07-21 23:08:54 +0300

    allow connecting custom repo at backend localrun

--- server-side/localrun.sh
+++ server-side/localrun.sh
@@ -1,9 +1,17 @@
 set -e
-cd tests
-./init_repo.sh
-cd ..
 PORT=8181
+REPO=$1
+
+if [ "$REPO" == "" ]; then
+    REPO="./tests/test_repo"
+    echo "WARNING! NO REPO SPECIFIED! USING TEST REPO AT: $REPO"
+    cd tests
+    ./init_repo.sh
+    cd ..
+fi
+
 echo "host_port=$PORT" > .env
+echo "target_repo=$REPO" > .env
 docker-compose up --build --detach
 echo "server-side is running locally. Check url:"
 echo "      http://localhost:$PORT/api/health"
--- server-side/testrun.sh
+++ server-side/testrun.sh
@@ -10,6 +10,7 @@ cd ../..
 set +e
 echo "head_commit=$commit" > .env
 echo "host_port=80" >> .env
+echo "target_repo=./tests/test_repo" >> .env
 docker-compose up --build --abort-on-container-exit
 RESULT=$?
 docker-compose logs > ./build_artifacts/compose.log
