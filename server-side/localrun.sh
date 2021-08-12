ba032b565e4f93427da308bd921a9b593867c975
ilya-t <extraletters@gmail.com>
2021-08-12 23:27:17 +0300

    localrun: don't run tests at specified repo

--- server-side/localrun.sh
+++ server-side/localrun.sh
@@ -10,10 +10,18 @@ if [ "$REPO" == "" ]; then
     cd ..
 fi
 
-echo "host_port=$PORT" > .env
-echo "target_repo=$REPO" >> .env
-echo "target_ssh_keys=~/.ssh" >> .env
-docker-compose up --build --detach
+target_ssh_keys=~/.ssh
+
+docker build ./app --tag wiki_backend
+docker run \
+    --detach \
+    --publish $PORT:80 \
+    --volume $REPO:/app/repo-store/repo \
+    --volume $target_ssh_keys:/root/.ssh \
+    --volume /tmp/test_repo.git:/tmp/test_repo.git \
+    wiki_backend:latest
+
+
 echo "server-side is running locally. Check url:"
 echo "      http://localhost:$PORT/api/health"
 echo "(to forward port to emulator use: adb root && adb forward tcp:$PORT tcp:80)"
