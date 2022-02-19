b8c4eeb1044b978f6272b3866ef89d844c499952
ilya-t <extraletters@gmail.com>
2022-02-19 12:02:25 +0300

    support flings at edit screen

--- client-side/app/src/main/java/com/tsourcecode/wiki/app/editor/EditorScreenController.kt
+++ client-side/app/src/main/java/com/tsourcecode/wiki/app/editor/EditorScreenController.kt
@@ -2,8 +2,14 @@ package com.tsourcecode.wiki.app.editor
 
 import android.text.Editable
 import android.text.TextWatcher
+import android.text.method.ScrollingMovementMethod
+import android.view.GestureDetector
+import android.view.GestureDetector.SimpleOnGestureListener
+import android.view.MotionEvent
 import android.view.View
-import android.widget.EditText
+import android.view.View.OnTouchListener
+import android.widget.Scroller
+import androidx.appcompat.widget.AppCompatEditText
 import androidx.lifecycle.MutableLiveData
 import com.tsourcecode.wiki.app.R
 import com.tsourcecode.wiki.app.documents.Document
@@ -20,6 +26,7 @@ import io.noties.markwon.editor.MarkwonEditorTextWatcher
 import io.noties.markwon.editor.handler.EmphasisEditHandler
 import io.noties.markwon.editor.handler.StrongEmphasisEditHandler
 import java.util.concurrent.Executors
+
 
 class EditorScreenController(
         navigator: ActivityNavigator,
@@ -42,20 +49,9 @@ class EditorScreenController(
     }
 
     private fun configureEditor(d: Document, container: View) {
-        val textView = container.findViewById<EditText>(R.id.tv_markwon)
-        val markwon = Markwon.create(container.context)
-        val editor = MarkwonEditor
-                .builder(markwon)
-                .useEditHandler(HeadingEditHandler())
-                .useEditHandler(EmphasisEditHandler())
-                .useEditHandler(StrongEmphasisEditHandler())
-                .useEditHandler(CodeEditHandler())
-                //                .useEditHandler(StrikethroughEditHandler())
-                //                .useEditHandler(BlockQuoteEditHandler())
-                //                .useEditHandler(CodeEditHandler())
-                .build()
-        textView.addTextChangedListener(MarkwonEditorTextWatcher.withPreRender(
-                editor, Executors.newSingleThreadExecutor(), textView));
+        val textView = container.findViewById<AppCompatEditText>(R.id.tv_markwon)
+        textView.configureMarkwon()
+        textView.configureScrolling()
 
         val md = docContentProvider.getContent(d)
         textView.setText(md.subSequence(0, md.length - 1))
@@ -68,5 +64,47 @@ class EditorScreenController(
                 documentsController.save(d, textView.text.toString())
             }
         })
+    }
+
+    private fun AppCompatEditText.configureScrolling() {
+        val scroller = Scroller(context)
+        setScroller(scroller)
+        isVerticalScrollBarEnabled = true
+        movementMethod = ScrollingMovementMethod()
+        setOnTouchListener(object : OnTouchListener {
+            // Could make this a field member on your activity
+            var gesture = GestureDetector(context, object : SimpleOnGestureListener() {
+                override fun onFling(e1: MotionEvent, e2: MotionEvent, velocityX: Float, velocityY: Float): Boolean {
+                    scroller.fling(0, scrollY, 0, (-velocityY).toInt(), 0, 0, 0, lineCount * lineHeight)
+                    return super.onFling(e1, e2, velocityX, velocityY)
+                }
+
+                override fun onDown(e: MotionEvent?): Boolean {
+                    scroller.abortAnimation()
+                    return false
+                }
+            })
+
+            override fun onTouch(v: View, event: MotionEvent): Boolean {
+                gesture.onTouchEvent(event)
+                return false
+            }
+        })
+    }
+
+    private fun AppCompatEditText.configureMarkwon() {
+        val markwon = Markwon.create(context)
+        val editor = MarkwonEditor
+                .builder(markwon)
+                .useEditHandler(HeadingEditHandler())
+                .useEditHandler(EmphasisEditHandler())
+                .useEditHandler(StrongEmphasisEditHandler())
+                .useEditHandler(CodeEditHandler())
+                //                .useEditHandler(StrikethroughEditHandler())
+                //                .useEditHandler(BlockQuoteEditHandler())
+                //                .useEditHandler(CodeEditHandler())
+                .build()
+        addTextChangedListener(MarkwonEditorTextWatcher.withPreRender(
+                editor, Executors.newSingleThreadExecutor(), this));
     }
 }
