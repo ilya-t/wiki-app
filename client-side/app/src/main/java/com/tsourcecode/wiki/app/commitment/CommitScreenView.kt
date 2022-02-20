a178ad95ba2c401e541aa2af47ee272da933c86d
ilya-t <extraletters@gmail.com>
2022-02-20 22:48:08 +0300

    turn on compose support

--- client-side/app/src/main/java/com/tsourcecode/wiki/app/commitment/CommitScreenView.kt
+++ client-side/app/src/main/java/com/tsourcecode/wiki/app/commitment/CommitScreenView.kt
@@ -1,6 +1,11 @@
 package com.tsourcecode.wiki.app.commitment
 
 import android.view.View
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -9,8 +14,11 @@ import com.tsourcecode.wiki.app.R
 import com.tsourcecode.wiki.app.navigation.ActivityNavigator
 import com.tsourcecode.wiki.app.navigation.Screen
 import com.tsourcecode.wiki.lib.domain.commitment.StatusModel
+import com.tsourcecode.wiki.lib.domain.commitment.StatusViewModel
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
+
+private const val useCompose = false
 
 class CommitScreenView(
         private val lifecycle: LifecycleOwner,
@@ -27,6 +35,7 @@ class CommitScreenView(
     }
 
     private fun setupScreen(root: View) {
+        val composeView = root.findViewById<ComposeView>(R.id.commit_container)
         val recyclerView = root.findViewById<RecyclerView>(R.id.commit_recycler)
         val commitAdapter = CommitAdapter(statusModel)
         recyclerView.adapter = commitAdapter
@@ -34,8 +43,28 @@ class CommitScreenView(
         recyclerView.layoutManager = LinearLayoutManager(root.context, LinearLayoutManager.VERTICAL, reverse)
         lifecycle.lifecycleScope.launch {
             statusModel.statusFlow.collect { viewModel ->
-                commitAdapter.viewModel = viewModel
+                if (useCompose) {
+                    composeView.setContent {
+                        ComposeCommitScreen(viewModel)
+                    }
+                } else {
+                    commitAdapter.viewModel = viewModel
+                }
             }
         }
     }
+}
+
+@Composable
+private fun ComposeCommitScreen(viewModel: StatusViewModel) {
+    Column {
+        Text(text = "msg.author")
+        Text(text = "msg.body")
+    }
+}
+
+@Preview
+@Composable
+fun PreviewCommitScreen() {
+    ComposeCommitScreen(StatusViewModel())
 }
