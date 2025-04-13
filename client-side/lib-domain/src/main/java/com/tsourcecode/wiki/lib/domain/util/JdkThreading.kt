4b0cb3433aafe1e8a004083dabc395cb512aa82d
ilya-t <extraletters@gmail.com>
2025-04-13 11:38:06 +0300

    client: isolate test environments

--- client-side/lib-domain/src/main/java/com/tsourcecode/wiki/lib/domain/util/JdkThreading.kt
+++ client-side/lib-domain/src/main/java/com/tsourcecode/wiki/lib/domain/util/JdkThreading.kt
@@ -1,9 +1,14 @@
 package com.tsourcecode.wiki.lib.domain.util
 
 import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.asCoroutineDispatcher
+import java.util.concurrent.Executors
 
 class JdkThreading : Threading {
-    override val main: CoroutineDispatcher = Dispatchers.Default
-    override val io: CoroutineDispatcher = Dispatchers.IO
+    override val main: CoroutineDispatcher = Executors
+        .newSingleThreadExecutor()
+        .asCoroutineDispatcher()
+    override val io: CoroutineDispatcher = Executors
+        .newCachedThreadPool()
+        .asCoroutineDispatcher()
 }
