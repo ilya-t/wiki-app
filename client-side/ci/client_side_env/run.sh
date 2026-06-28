d646f3e32b071fd7ae7594eac94efed274176c91
ilya-t <extraletters@gmail.com>
2026-06-28 10:52:01 +0300

    attach test logs at ci

--- client-side/ci/client_side_env/run.sh
+++ client-side/ci/client_side_env/run.sh
@@ -1,4 +1,13 @@
-set -e
+set +e
+
+rm -rf /tmp/build_artifacts
+mkrdir -p /tmp/build_artifacts
+
 ./gradlew test #lint
+TEST_EXIT=$?
+
+set -e
+
 cp -R app/build/reports /tmp/build_artifacts/app
 cp -R lib-domain/build/reports /tmp/build_artifacts/lib-domain
+exit $TEST_EXIT
