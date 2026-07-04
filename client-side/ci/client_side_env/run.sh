51e0278aaa5f8d5c9938cdb450b45af793948812
ilya-t <extraletters@gmail.com>
2026-07-04 16:43:35 +0300

    attach apk as artifact

--- client-side/ci/client_side_env/run.sh
+++ client-side/ci/client_side_env/run.sh
@@ -4,9 +4,12 @@ set +e
 mkdir -p /tmp/build_artifacts
 rm -rf /tmp/build_artifacts/*
 
-./gradlew test > /tmp/build_artifacts/gradle.log 2>&1
+./gradlew test :app:assembleDebug > /tmp/build_artifacts/gradle.log 2>&1
 TEST_EXIT=$?
 
+if [ -f app/build/outputs/apk/debug/app-debug.apk ]; then
+    cp app/build/outputs/apk/debug/app-debug.apk /tmp/build_artifacts/
+fi
 if [ -d app/build/reports ]; then
     cp -R app/build/reports /tmp/build_artifacts/app
 fi
