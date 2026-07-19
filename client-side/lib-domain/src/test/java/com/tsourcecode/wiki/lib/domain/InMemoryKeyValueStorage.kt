7c7344be3192f63bf1d678355674a00fbe7d38df
ilya-t <extraletters@gmail.com>
2026-07-19 11:27:43 +0300

    cover case: pre-commit hooks usage

--- client-side/lib-domain/src/test/java/com/tsourcecode/wiki/lib/domain/InMemoryKeyValueStorage.kt
+++ client-side/lib-domain/src/test/java/com/tsourcecode/wiki/lib/domain/InMemoryKeyValueStorage.kt
@@ -4,11 +4,15 @@ import com.tsourcecode.wiki.lib.domain.storage.KeyValueStorage
 import java.util.concurrent.ConcurrentHashMap
 
 class InMemoryKeyValueStorage : KeyValueStorage {
-    private val store = ConcurrentHashMap<String, String?>()
+    private val store = ConcurrentHashMap<String, String>()
     override fun get(key: String): String? = store[key]
 
     override fun set(key: String, value: String?) {
-        store[key] = value
+        if (value == null) {
+            store.remove(key)
+        } else {
+            store[key] = value
+        }
     }
 
     override fun contains(key: String): Boolean = store.contains(key)
